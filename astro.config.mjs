daf96985ae63014a989c984d02e57e06814f33c9
Muhammad-Hazimi-Yusri <muhammadhazimiyusri@gmail.com>
2025-12-02 23:49:46 +0800

    fix: commented github pages static site in astro.config.mjs and simplified deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://Muhammad-Hazimi-Yusri.github.io',
-  base: '/eee-roadmap',
+  // site: 'https://Muhammad-Hazimi-Yusri.github.io',
+  // base: '/eee-roadmap',
+  // When using a custom subdomain like eee-roadmap.muhammadhazimiyusri.uk,
+  // the base path should be set to the root (/) or an empty string.
+  base: '/', 
+  // ... other configs like adapter, etc.
 });
